f3bdda583f352d82939b995884338cbf41fab2ff
Diana <dianagren@gmail.com>
2013-05-25 11:43:10 +0200

    Fetch correct answer to question.

--- backend/edit_questions.js
+++ backend/edit_questions.js
@@ -8,9 +8,18 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+function fetch_answers(question_id) {
+	var sqlString = 'SELECT answers.question_id, answers.id, questions.answer_id, answers.text FROM answers, questions WHERE answers.question_id = ' + question_id + ' AND answers.id = questions.answer_id';
+	connection.query(sqlString, function(err, rows, fields) {
+		if (err) throw err;
+		console.log('Answer: ', rows[0].text);
+		connection.end();
+	});
+}
+
 connection.query('SELECT * FROM questions ORDER BY RAND() LIMIT 1', function(error, rows, fields) {
 	if (error) throw error;
 	console.log("The solution is: ", rows[0].text);
+	fetch_answers(rows[0].id);
 });
 
-connection.end();
