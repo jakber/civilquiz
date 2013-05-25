4091c3a45d00d06eed9c35cbcc06443c0381ca9f
jakob <jakob@videoplaza.com>
2013-05-25 17:32:41 +0200

    support for stand-alone client mode.

---
+++ backend/public/scripts/config.js
@@ -0,0 +1,2 @@
+//var getQuestion = getQuestionFile;
+var getQuestion = getQuestionDatabase;
---
+++ backend/public/scripts/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+	{
+		id: 1,
+		text: "Vad bör du elda med inomhus?",
+		answers: [
+			{id: "a", text: "Torr ved i lämplig storlek", correct:1},
+			{id: "b", text: "Fuktig ved i lämplig storlek", correct:0},
+			{id: "c", text: "Torr ved i olämplig storlek", correct:0},
+			{id: "d", text: "Fuktig ved i olämplig storlek", correct:0}
+		]
+	},
+	{
+		id: 2,
+		text: "Inomhustemperaturen sjunker om vi inte har tillgång till uppvärmning. I ett ordinärt välbyggt småhus med en inomhustemperatur på +21 grader tar det ett antal timmar innan inomhustemperaturen har sjunkit till  +10 grader, förutsatt att utomhustemperaturen är -10 grader. Hur lång tid tar det?",
+		answers: [
+			{id: 1, text: "24 timmar", correct:0},
+			{id: 2, text: "36 timmar", correct:1},
+			{id: 3, text: "48 timmar", correct:0},
+			{id: 4, text: "60 timmar", correct:0}
+		]
+	}
+];
