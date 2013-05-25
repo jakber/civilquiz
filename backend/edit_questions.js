d8418fb51a85e68a7c969cad42ea271fc493e9bb
Diana <dianagren@gmail.com>
2013-05-25 12:23:11 +0200

    Add generic-pool.

--- backend/edit_questions.js
+++ backend/edit_questions.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var poolModule = require('generic-pool');
 var connection = mysql.createConnection({
 	host : '127.0.0.1',
 	user : 'root',
@@ -6,20 +7,66 @@ var connection = mysql.createConnection({
 	database : 'civil_quiz',
 });
 
-connection.connect();
+var dbPool = poolModule.Pool({
+	name: 'mysql',
+	create: function(callback) {
+		connection.connect();
+		connection.on('error', function(err) {
+			if (!err.fatal) {
+		      return;
+		    }
+		    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+		      throw err;
+		    }
+			dbPool.destroy(connection);
+			console.log("Removed erroneous connection from pool.");
+		});
+		callback(null, connection);
+	},	
+	destroy: function(connection){ connection.end();},
+	max: 4,
+	idleTimeoutMillis: 30000
+});
 
-function fetch_answers(question_id) {
-	var sqlString = 'SELECT answers.question_id, answers.id, questions.answer_id, answers.text FROM answers, questions WHERE answers.question_id = ' + question_id + ' AND answers.id = questions.answer_id';
-	connection.query(sqlString, function(err, rows, fields) {
-		if (err) throw err;
-		console.log('Answer: ', rows[0].text);
-		connection.end();
+function bitToBoolean(field, next) {
+	if (field.type == "BIT") {
+		return (field.buffer()[0] == 1);
+	}
+	return next();
+}
+function execDbQuery(query,params,queryCallback) {
+	dbPool.acquire(function(err, client) {			
+		if(err) {
+			console.log("Error while acquiring db resource");
+		} else {
+			client.query({"sql": query, "typeCast": bitToBoolean, "values":params}, function selectCb(err, results, fields) {
+				dbPool.release(client);
+				queryCallback(err, results, fields);
+			});
+		}	
 	});
 }
 
-connection.query('SELECT * FROM questions ORDER BY RAND() LIMIT 1', function(error, rows, fields) {
-	if (error) throw error;
-	console.log("The solution is: ", rows[0].text);
-	fetch_answers(rows[0].id);
+function fetch_answers(question_id) {
+	var sqlString = 'SELECT answers.question_id, answers.id, questions.answer_id, answers.text FROM answers, questions WHERE answers.question_id = ' + question_id;
+	execDbQuery(sqlString, null, function(err, res) {
+		if (err) throw err;
+		console.log('Answer: ', res[0].text);
+	});
+}
+
+var express = require('express');
+var app = express();
+app.get('/question', function(request, response) {
+	fetchQuestion();
 });
 
+function fetchQuestion() {
+	var sqlString = 'SELECT * FROM questions ORDER BY RAND() LIMIT 1';
+	execDbQuery(sqlString, null, function(err, res) {
+		if (err) throw err;
+		console.log("Question: ", res[0].text);
+		fetch_answers(res[0].id);
+	});
+}
+fetchQuestion();
